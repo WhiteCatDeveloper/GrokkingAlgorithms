96a852479aaae96803446321d87fe061ae929c91
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-07-11 22:41:41 +0400

    Начало работы с быстрой сортировкой

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,4 +1,6 @@
 package sort;
 
 public class BubbleSort {
+
+
 }
--- src/sort/recursion/FastSort.java
+++ src/sort/recursion/FastSort.java
@@ -1,5 +1,28 @@
 package sort.recursion;
 
-public class FastSort {
+import java.util.Arrays;
 
+public class FastSort {
+    public static void main(String[] args) {
+        int[] ints = new int[12];
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] =(int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(ints));
+    }
+
+    private static int[] fastSort(int[] arr) {
+        if (arr.length < 2) return arr;
+        else {
+            //опорный элемент
+            int pivot = arr[0];
+            //подмассив всех элементов меньше опорного
+            int[] less =
+            //подмассив элементов больше опорного
+            int[] greater =
+            //надо как то сложить их)) и передать в рекурсивный вызов дальше
+            return fastSort(less + pivot + greater);
+        }
+    }
 }
