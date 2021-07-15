aca15ca8b602ff32f5e65325ab0fa0a0b7dd8143
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-07-15 22:26:12 +0400

    в other.Main быстрая сортировка, взятая из интернета. Реализована сортировка пузырьком(нужно немного доделать)

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -12,10 +12,10 @@ public class BubbleSort {
     // Нужно отполировать, чтобы небыло двойной работы
     private static int[] bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    int buff = arr[i];
-                    arr[i] = arr[j];
+            for (int j = arr.length -1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int buff = arr[j -1];
+                    arr[j -1] = arr[j];
                     arr[j] = buff;
                 }
             }
