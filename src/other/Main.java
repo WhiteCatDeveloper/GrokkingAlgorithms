f52f3167ce92fbb88079c98b4cb92d9e099d80dc
Whitecatsdeveloper <whitecatsdeveloper@gmail.com>
2021-07-25 23:33:20 +0400

    Работа с "очередью"

--- src/other/Main.java
+++ src/other/Main.java
@@ -4,6 +4,21 @@ package other;
 
 public class Main {
     public static void main(String[] args) {
-
+        MyQueue queue = new MyQueue();
+        String s = "abc";
+        String s1 = "abcd";
+        String s2 = "abcdf";
+        String s3 = "abcdfe";
+        String s4 = "abcdefg";
+        queue.add(s);
+        queue.add(s1);
+        queue.add(s2);
+        queue.add(s3);
+        queue.add(s4);
+        System.out.println(queue.remote());
+        System.out.println(queue.remote());
+        System.out.println(queue.remote());
+        System.out.println(queue.remote());
+        System.out.println(queue.remote());
     }
 }
--- src/other/MyQueue.java
+++ src/other/MyQueue.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class MyQueue{
     private final List<Object> list = new ArrayList<>();
 
-    private void add (Object obj) {
+    public void add (Object obj) {
         list.add(obj);
     }
 
-    private Object remote() {
+    public Object remote() {
         Object result = list.get(0);
         list.remove(list.get(0));
         return result;
